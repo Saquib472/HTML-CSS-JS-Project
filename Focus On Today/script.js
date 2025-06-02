c30d6bd6241caa9c92c6c81ea44dc37a351404f2
Najmush Saquib Ali <87539496+Saquib472@users.noreply.github.com>
2025-06-02 23:04:45 +0530

    Updated
    Without Local Storage but added the functionality of Add Button

--- Focus On Today/script.js
+++ Focus On Today/script.js
@@ -1,9 +1,9 @@
-const customCheckboxList = document.querySelectorAll('.custom-checkbox')
-const inputFields = document.querySelectorAll('.goal-input')
+let customCheckboxList = document.querySelectorAll('.custom-checkbox')
+let inputFields = document.querySelectorAll('.goal-input')
 const errorLabel = document.querySelector('.error-label')
-const progressValue = document.querySelector('.progress-value')
-const goalContainerSec = document.querySelector('.goal-container-sec')
-const goalContainer = document.querySelector('.goal-container')
+let progressValue = document.querySelector('.progress-value')
+let goalContainerSec = document.querySelector('.goal-container-sec')
+let goalContainer = document.querySelector('.goal-container')
 const addBtn = document.querySelector('.addbtn')
 let pValue = localStorage.getItem('pValue') || 0
 progressValue.style.width = pValue + '%'
@@ -11,102 +11,157 @@ const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
 let completedGoals = Number(localStorage.getItem('completedGoals')) || 0
 progressValue.childNodes[0].innerText = `${completedGoals}/${inputFields.length} Completed`
 
-customCheckboxList.forEach((checkbox)=>{
-    if(Object.keys(allGoals).length > 0){
-        if(allGoals[checkbox.nextElementSibling.id]['completed']){
-            checkbox.parentElement.classList.add('completed')
-        }else{
-            checkbox.parentElement.classList.remove('completed')
-        }
-    }
-    checkbox.addEventListener('click', (e)=>{
-        const allpass = [...inputFields].every(node =>{
-            return node.value != ''
-        })  
-        if(allpass){
-            checkbox.parentElement.classList.toggle('completed')
+function checkBoxChecking() {
+    customCheckboxList.forEach((checkbox) => {
+        if ([...checkbox.classList].includes('exist')) {
             if([...checkbox.parentElement.classList].includes('completed')){
-                allGoals[checkbox.nextElementSibling.id]['completed'] = true
-                localStorage.setItem('allGoals', JSON.stringify(allGoals))
-                completedGoals +=1
-                localStorage.setItem('completedGoals', completedGoals)
-                progressValue.childNodes[0].innerText = `${completedGoals}/${inputFields.length} Completed`
-                pValue = completedGoals/inputFields.length * 100
-                progressValue.style.width = pValue + '%'
-                localStorage.setItem('pValue', pValue)
-            }else{
-                allGoals[checkbox.nextElementSibling.id]['completed'] = false
-                localStorage.setItem('allGoals', JSON.stringify(allGoals))
-                completedGoals -=1
-                localStorage.setItem('completedGoals', completedGoals)
-                progressValue.childNodes[0].innerText = `${completedGoals}/${inputFields.length} Completed`
-                pValue = completedGoals/inputFields.length * 100
-                progressValue.style.width = pValue + '%'
-                localStorage.setItem('pValue', pValue)
+                checkbox.parentElement.classList.remove('completed')
             }
-        }else{
-            errorLabel.parentElement.classList.add('show-error')
+        } else {
+            // if (Object.keys(allGoals).length > 0) {
+            //     console.log(allGoals)
+            //     console.log(checkbox.nextElementSibling.id)
+            //     if (allGoals[checkbox.nextElementSibling.id]['completed']) {
+            //         checkbox.parentElement.classList.add('completed')
+            //     } else {
+            //         checkbox.parentElement.classList.remove('completed')
+            //     }
+            // }
+            checkbox.classList.add('exist')
+            checkbox.addEventListener('click', (e) => {
+                console.log('clicked')
+                const allpass = [...inputFields].every(node => {
+                    return node.value != ''
+                })
+                if (allpass) {
+                    checkbox.parentElement.classList.toggle('completed')
+                    if ([...checkbox.parentElement.classList].includes('completed')) {
+                        console.log(checkbox.nextElementSibling.id)
+                        // console.log(allGoals)
+                        // allGoals[checkbox.nextElementSibling.id]['completed'] = true
+                        // localStorage.setItem('allGoals', JSON.stringify(allGoals))
+                        completedGoals += 1
+                        // localStorage.setItem('completedGoals', completedGoals)
+                        progressValue.childNodes[0].innerText = `${completedGoals}/${inputFields.length} Completed`
+                        pValue = completedGoals / inputFields.length * 100
+                        progressValue.style.width = pValue + '%'
+                        // localStorage.setItem('pValue', pValue)
+                    } else {
+                        // allGoals[checkbox.nextElementSibling.id]['completed'] = false
+                        // localStorage.setItem('allGoals', JSON.stringify(allGoals))
+                        completedGoals -= 1
+                        // localStorage.setItem('completedGoals', completedGoals)
+                        progressValue.childNodes[0].innerText = `${completedGoals}/${inputFields.length} Completed`
+                        pValue = completedGoals / inputFields.length * 100
+                        progressValue.style.width = pValue + '%'
+                        // localStorage.setItem('pValue', pValue)
+                    }
+                } else {
+                    errorLabel.parentElement.classList.add('show-error')
+                    errorLabel.innerHTML = `Please Set All ${inputFields.length} Goals`
+                }
+            })
         }
     })
-})
+}
+checkBoxChecking()
 
-inputFields.forEach(el =>{
-    if(Object.keys(allGoals).length > 0){  
-        el.value = allGoals[el.id]['goalName']
+function inputFieldsCheck() {
+    console.log('intu')
+    inputFields.forEach(el => {
+        if ([...el.classList].includes('exist')) {
+            console.log('true')
+        } else {
+            console.log('herererererererererererererereererererererer')
+            el.classList.add('exist')
+            // if (Object.keys(allGoals).length > 0) {
+            //     el.value = allGoals[el.id]['goalName']
+            // }
+            el.addEventListener('focus', (e) => {
+                errorLabel.parentElement.classList.remove('show-error')
+            })
+            el.addEventListener('keydown', (e) => {
+                if ([...el.parentElement.classList].includes('completed')) {
+                    e.preventDefault()
+                }
+            })
+            el.addEventListener('input', (e) => {
+                console.log('here')
+                if ([...el.parentElement.classList].includes('completed')) {
+                    e.preventDefault()
+                } else {
+                    allGoals[el.id] = {
+                        goalName: el.value,
+                        completed: false
+                    }
+                    // localStorage.setItem('allGoals', JSON.stringify(allGoals))
+                }
+            })
+        }
+    })
+
+}
+inputFieldsCheck()
+function makeid() {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 5; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    el.addEventListener('focus', (e)=>{
-        errorLabel.parentElement.classList.remove('show-error')
-    })
-    el.addEventListener('keydown', (e)=>{
-        if([...el.parentElement.classList].includes('completed')){
-            e.preventDefault()
-        }
-    })
-    el.addEventListener('input', (e)=>{
-        if([...el.parentElement.classList].includes('completed')){
-            e.preventDefault()
-        }else{
-            allGoals[el.id] = {
-                goalName : el.value,
-                completed : false
-            }
-            localStorage.setItem('allGoals', JSON.stringify(allGoals))
-        }
-    })
-})
+    return result;
+}
 
-let inputFieldsAfterAdd = inputFields.length
-let addedExtraGoals = 0
-let addedExtraGoalOrNot = false
-addBtn.addEventListener('click', (e)=>{
-    addedExtraGoalOrNot = true
-    localStorage.setItem('addedExtraGoalOrNot' , addedExtraGoalOrNot)
-    ++addedExtraGoals
-    localStorage.setItem('addedExtraGoals', addedExtraGoals)
-    ++inputFieldsAfterAdd
-    localStorage.setItem('inputFieldsAfterAdd', inputFieldsAfterAdd)
+// let inputFieldsAfterAdd = inputFields.length
+// let addedExtraGoals = 0
+// let addedExtraGoalOrNot = false
+addBtn.addEventListener('click', (e) => {
+    // addedExtraGoalOrNot = true
+    // localStorage.setItem('addedExtraGoalOrNot' , addedExtraGoalOrNot)
+    // ++addedExtraGoals
+    // localStorage.setItem('addedExtraGoals', addedExtraGoals)
+    // ++inputFieldsAfterAdd
+    // localStorage.setItem('inputFieldsAfterAdd', inputFieldsAfterAdd)
 
+    // const copyNode = goalContainer.cloneNode(true)
+    // copyNode.classList.remove('completed')
+    // copyNode.lastElementChild.value = ''
+    // copyNode.lastElementChild.id = inputFieldsAfterAdd == 4 ? 'fourth' : 'fifth'
+    // goalContainerSec.appendChild(copyNode)   
+    // progressValue.childNodes[0].innerText = `${completedGoals}/${inputFieldsAfterAdd} Completed`
+    // pValue = completedGoals/(inputFieldsAfterAdd) * 100
+    // progressValue.style.width = pValue + '%'
+    // goalContainerSec.lastElementChild.firstElementChild.addEventListener('click', (e)=>{
+    //     const allpass = [...inputFields].every(node =>{
+    //         return node.value != ''
+    //     })
+    //     const currentInputField =  copyNode.lastElementChild.id == 'fourth' && goalContainerSec.firstElementChild.lastElementChild.value ? true : copyNode.lastElementChild.id == 'fifth' && goalContainerSec.lastElementChild.lastElementChild.value ? true : false
+    //     if(allpass && currentInputField){
+    //         console.log('here')
+    //     }else{
+    //         errorLabel.parentElement.classList.add('show-error')
+    //         console.log('herererererer')
+    //     }
+
+    // })
+    let randomID = makeid()
     const copyNode = goalContainer.cloneNode(true)
     copyNode.classList.remove('completed')
+    copyNode.firstElementChild.classList.remove('exist')
+    copyNode.lastElementChild.classList.remove('exist')
     copyNode.lastElementChild.value = ''
-    copyNode.lastElementChild.id = inputFieldsAfterAdd == 4 ? 'fourth' : 'fifth'
-    goalContainerSec.appendChild(copyNode)   
-    console.log(copyNode)
-    console.log(goalContainerSec.lastElementChild)
-    progressValue.childNodes[0].innerText = `${completedGoals}/${inputFieldsAfterAdd} Completed`
-    pValue = completedGoals/(inputFieldsAfterAdd) * 100
-    progressValue.style.width = pValue + '%'
-    console.log(goalContainerSec.lastElementChild.firstElementChild)
-    goalContainerSec.lastElementChild.firstElementChild.addEventListener('click', (e)=>{
-        const allpass = [...inputFields].every(node =>{
-            return node.value != ''
-        })
-        const currentInputField =  copyNode.lastElementChild.id == 'fourth' && !goalContainerSec.firstElementChild.lastElementChild.value ? true : copyNode.lastElementChild.id == 'fifth' && !goalContainerSec.lastElementChild.lastElementChild.value ? true : false
-        if(allpass && currentInputField){
-            
-        }else{
-            errorLabel.parentElement.classList.add('show-error')
-        }
+    copyNode.lastElementChild.id = randomID
+    goalContainerSec.appendChild(copyNode)
 
-    })
+    customCheckboxList = document.querySelectorAll('.custom-checkbox')
+    inputFields = document.querySelectorAll('.goal-input')
+    progressValue = document.querySelector('.progress-value')
+    goalContainer = document.querySelector('.goal-container')
+    pValue = 0
+    progressValue.style.width = pValue + '%'
+    completedGoals = 0
+    errorLabel.innerHTML = `Please Set All ${inputFields.length} Goals`
+    progressValue.childNodes[0].innerText = `${completedGoals}/${inputFields.length} Completed`
+    checkBoxChecking()
+    inputFieldsCheck()
 })
